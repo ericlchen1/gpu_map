f3425b81e58b92ed8497707fa135bab89bab6775
ericlchen <ericlchen@utexas.edu>
2021-12-26 22:43:32 -0600

    Added basic home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
@@ -16,22 +15,6 @@ function AllRoutes () {
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <Router>
       <Navbar></Navbar>
       <AllRoutes></AllRoutes>
--- front-end/src/views/Home/Home.js
+++ front-end/src/views/Home/Home.js
@@ -1,9 +1,48 @@
+import React, { useState, useEffect, useRef } from "react"
+import { DownOutlined } from "@ant-design/icons"
+import { Typography } from 'antd';
+import './Home.css';
 
+
+const { Title, Paragraph } = Typography;
 
 function Home () {
+    const ref = useRef(null);
+
     return (
-        <>
-        </>
+        <div>
+            <div className="splashImage">
+                <div className="splashContent">
+                    <div className="headerText">
+						<Title level={1} className="title">
+							GPU Map
+						</Title>
+						<Paragraph className="about">
+							Find GPUs near you! Search and give information
+                            on available GPUs to help those looking for GPUs.
+						</Paragraph>
+                    </div>
+                </div>
+                <div className="downButton">
+					<DownOutlined
+						className="downButtonIcon"
+						onClick={() => {
+							window.scrollTo({
+								top: ref.current.offsetTop,
+								behavior: "smooth",
+							})
+						}}
+					/>
+				</div>
+            </div>
+            <div className="modelCards" ref={ref}>
+                <Title level={1} style={{ textAlign: "center", marginTop: 32 }}>
+                    Recent News
+                </Title>
+				<div className="cardFlexContainer">
+                </div>
+            </div>
+        </div>
     )
 }
 
