32b24a9df8d43f02e7086153b6a981014313fed3
ericlchen <ericlchen@utexas.edu>
2021-12-27 17:25:08 -0600

    Added Home learn more section

--- front-end/src/views/Home/Home.js
+++ front-end/src/views/Home/Home.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect, useRef } from "react"
 import { DownOutlined } from "@ant-design/icons"
-import { Typography } from 'antd';
+import { Card, Typography } from 'antd';
 import './Home.css';
+// import { Link } from "react-router-dom";
 
+import Nvidia from "./images/nvidia_logo.jpg";
+import AMD from "./images/amd_logo.webp";
 
 const { Title, Paragraph } = Typography;
 
@@ -18,7 +21,7 @@ function Home () {
 							GPU Map
 						</Title>
 						<Paragraph className="about">
-							Find GPUs near you! Search and give information
+							Find GPUs near you! Search and contribute information
                             on available GPUs to help those looking for GPUs.
 						</Paragraph>
                     </div>
@@ -37,9 +40,51 @@ function Home () {
             </div>
             <div className="modelCards" ref={ref}>
                 <Title level={1} style={{ textAlign: "center", marginTop: 32 }}>
-                    Recent News
+                    Learn More
                 </Title>
 				<div className="cardFlexContainer">
+                    <a href="https://www.nvidia.com/en-us/geforce/" className="card">
+                        <Card
+                            bordered={true}
+							className="card"
+							cover={
+								<img
+									className="cardImage"
+									alt={"Nvidia"}
+									src={Nvidia}
+								></img>
+							}
+							hoverable={true}
+                        >
+                            <Title level={2} style={{ textAlign: "center" }}>
+								Nvidia
+							</Title>
+							<Paragraph>
+								Learn more about current and upcoming Nvidia Geforce products.
+							</Paragraph>
+                        </Card>
+                    </a>
+                    <a href="https://www.amd.com/en/graphics/radeon-rx-graphics" className="card">
+                        <Card
+                            bordered={true}
+							className="card"
+							cover={
+								<img
+									className="cardImage"
+									alt={"AMD"}
+									src={AMD}
+								></img>
+							}
+							hoverable={true}
+                        >
+                            <Title level={2} style={{ textAlign: "center" }}>
+								AMD
+							</Title>
+							<Paragraph>
+								Learn more about AMD's Radeon RX graphics cards.
+							</Paragraph>
+                        </Card>
+                    </a>
                 </div>
             </div>
         </div>
