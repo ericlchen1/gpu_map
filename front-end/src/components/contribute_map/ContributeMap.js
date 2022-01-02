c2eeb4140e3252e0de863ba1e77d5d1e68945e43
ericlchen <ericlchen@utexas.edu>
2022-01-02 16:06:02 -0600

    Added using own location

--- front-end/src/components/contribute_map/ContributeMap.js
+++ front-end/src/components/contribute_map/ContributeMap.js
@@ -3,27 +3,111 @@ import {
     GoogleMap,
     useLoadScript,
     Marker,
-    InfoWindow,
+    // InfoWindow,
 } from "@react-google-maps/api";
 
 import MapStyles from "./MapStyles";
 
-// import { formatRelative } from 
+import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete";
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
+import styles from "./ContributeMap.module.css";
+import { Button, Col, Row } from "antd";
+
 
 const libraries = ["places"];
+
+// Styles for the map
 const mapContainerStyle = {
     width: "60vw",
     height: "60vh",
 };
+
+// Auto places center of map in Austin, TX
 const center = {
     lat: 30.267153,
     lng: -97.743061
 };
+
+// Change map styles
 const options = {
     styles: MapStyles,
     disableDefaultUI: true,
     zoomControl: true
 };
+
+function UseOwnLocation({panTo}) {
+    return (
+        <Button className={styles.locate} onClick={() => {
+            navigator.geolocation.getCurrentPosition((position) => {
+                panTo({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                });
+            }, () => null);
+        }}>
+            Use my location
+        </Button>
+    );
+}
+
+function MapSearchBar ({panTo}) {
+    const {
+        ready, 
+        value, 
+        suggestions: {status, data}, 
+        setValue, 
+        clearSuggestions,
+    } = usePlacesAutocomplete({
+        requestOptions: {   // location preference for search results
+            location: {lat: () => 30.267153, lng: () => -97.743061},
+            radius: 200 * 1000,
+        }
+    });
+
+    return (
+        <div className={styles.search}>
+            <Combobox 
+                onSelect={async (address) => {
+                    setValue(address, false);
+                    clearSuggestions();
+
+                    try {
+                        const result = await getGeocode({address});
+                        const { lat, lng } = await getLatLng(result[0]);
+                        panTo({lat, lng});
+                    } catch(error) {
+                        console.log("Error!")
+                    }
+                }}
+            >
+                <ComboboxInput 
+                    value={value} 
+                    onChange={(e) => {
+                        setValue(e.target.value)
+                    }}
+                    disabled={!ready}
+                    placeholder="Enter an address"
+                />
+                <ComboboxPopover>
+                    <ComboboxList>
+                    {status === "OK" && 
+                        data.map(({id, description}) => (
+                            <ComboboxOption key={id} value={description}/>
+                        ))}
+                    </ComboboxList>
+                </ComboboxPopover>
+            </Combobox>
+        </div>
+    );
+}
 
 function ContributeMap ({ onLocationChange }) {
     // console.log('map rerender')
@@ -48,11 +132,20 @@ function ContributeMap ({ onLocationChange }) {
         mapRef.current = map;
     }, []);
 
+    const panTo = useCallback(({lat, lng}) => {
+        mapRef.current.panTo({lat, lng});
+        mapRef.current.setZoom(14);
+    }, [])
+
     if (loadError) return "Error loading maps";
     if (!isLoaded) return "Loading maps";
 
     return (
         <div>
+            <Row>
+                <MapSearchBar panTo={panTo}/>
+                <UseOwnLocation panTo={panTo}/>
+            </Row>
             <GoogleMap
                 mapContainerStyle={mapContainerStyle}
                 zoom={10}
